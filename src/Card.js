a557401eae4a1e3f99a4f9dae28b4cf46071e6ca
Tristan Kantner <80457025+Tantris27@users.noreply.github.com>
2023-04-03 22:31:37 +0200

    Create Card, CardList and Searchbox Components

--- src/Card.js
+++ src/Card.js
@@ -3,13 +3,10 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   // const { name, email, id } = props
   return (<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-    <h1>RobotFriend</h1>
+    <img src={`https://robohash.org/${id}?200x200`} alt="Robots" />
     <div>
-      <img src={`https://robohash.org/${id}?200x200`} alt="Robots" />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
   </div>
   )
