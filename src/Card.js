9cb84198b35b9871c8cfa7ea2e7e01bc2f640b41
Tristan Kantner <80457025+Tantris27@users.noreply.github.com>
2023-04-03 20:24:07 +0200

    Create RoboCard Component

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Card = () => {
-  return (<>
+const Card = ({ name, email, id }) => {
+  // const { name, email, id } = props
+  return (<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
     <h1>RobotFriend</h1>
     <div>
-      <img src="https://robohash.org/test?200x200" alt="Robots" />
+      <img src={`https://robohash.org/${id}?200x200`} alt="Robots" />
       <div>
-        <h2>Robot Name</h2>
-        <p>robot@email.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
-  </>
+  </div>
   )
 }
 
